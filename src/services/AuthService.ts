a07837de2a3e21768ad63ffb4f28cfec466cffdc
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-02 21:16:21 +0530

    refactor separate logic for access and refresh token

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -31,34 +31,43 @@ export class AuthService {
         }
     }
 
-    async generateTokens(
-        user: User,
-    ): Promise<{ accessToken: string; refreshToken: string }> {
+    private generateAccessToken(payload: JwtPayload): string {
         const privateKey = this.getPrivateKey();
-
-        const payload: JwtPayload = {
-            sub: String(user.id),
-            role: user.role,
-        };
-
-        const accessToken = jwt.sign(payload, privateKey, {
+        return jwt.sign(payload, privateKey, {
             algorithm: 'RS256',
             expiresIn: '1h',
             issuer: 'auth-service',
         });
+    }
+
+    private async generateRefreshToken(
+        payload: JwtPayload,
+        user: User,
+    ): Promise<string> {
+        const refreshTokenSecret = Config.REFRESH_TOKEN_SECRET || 'my-secret';
 
         // Persist refresh token in database
         const newRefreshToken = await this.saveInDb(user);
 
-        const refreshTokenSecret: string =
-            Config.REFRESH_TOKEN_SECRET || 'my-secret';
-
-        const refreshToken = jwt.sign(payload, refreshTokenSecret, {
+        return jwt.sign(payload, refreshTokenSecret, {
             algorithm: 'HS256',
             expiresIn: '1y',
             issuer: 'auth-service',
             jwtid: String(newRefreshToken.id),
         });
+    }
+
+    async generateTokens(
+        user: User,
+    ): Promise<{ accessToken: string; refreshToken: string }> {
+        const payload: JwtPayload = {
+            sub: String(user.id),
+            role: user.role,
+            issuer: 'auth-service',
+        };
+
+        const accessToken = this.generateAccessToken(payload);
+        const refreshToken = await this.generateRefreshToken(payload, user);
 
         return { accessToken, refreshToken };
     }
@@ -85,10 +94,9 @@ export class AuthService {
 
     async saveInDb(user: User): Promise<RefreshToken> {
         const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
-        const newRefreshToken = this.refreshTokenRepository.save({
+        return this.refreshTokenRepository.save({
             user: user,
             expiresAt: new Date(Date.now() + MS_IN_YEAR),
         });
-        return newRefreshToken;
     }
 }

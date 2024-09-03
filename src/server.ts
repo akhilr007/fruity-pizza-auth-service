f55cfc1c76e15a6d027af4cf371c71c8ec3aaaa4
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-03 15:43:19 +0530

    add db initialise

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 import app from './app';
 import { Config } from './configs';
+import { AppDataSource } from './configs/data-source';
 import logger from './configs/logger';
 
-const startServer = () => {
+const startServer = async () => {
     const PORT = Config.PORT;
     try {
+        await AppDataSource.initialize();
+        logger.info('Database is connected successfully');
         app.listen(PORT, () => logger.info(`Listening on PORT : ${PORT}`));
     } catch (err) {
         if (err instanceof Error) {
             logger.error(err.message);
         }
-        process.exit(1);
+        setTimeout(() => {
+            process.exit(1);
+        }, 1000);
     }
 };
 

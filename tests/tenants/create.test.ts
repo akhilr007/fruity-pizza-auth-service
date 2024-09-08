94135488b097b235a3eca6509ee04f82b0d70f91
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-08 15:25:11 +0530

    add test for persisting tenant in database

--- tests/tenants/create.test.ts
+++ tests/tenants/create.test.ts
@@ -4,6 +4,7 @@ import { DataSource } from 'typeorm';
 
 import app from '../../src/app';
 import { AppDataSource } from '../../src/configs/data-source';
+import { Tenant } from '../../src/entity/Tenant';
 
 describe('POST /api/v1/tenants', () => {
     let connection: DataSource;
@@ -26,7 +27,7 @@ describe('POST /api/v1/tenants', () => {
         it('should return 201 status codes', async () => {
             const tenantData = {
                 name: 'Alex Foods',
-                address: 'password',
+                address: 'New York',
             };
 
             const response = await request(app)
@@ -34,6 +35,24 @@ describe('POST /api/v1/tenants', () => {
                 .send(tenantData);
 
             expect(response.statusCode).toBe(StatusCodes.CREATED);
+        });
+
+        it('should create a tenant in database', async () => {
+            const tenantData = {
+                name: 'Alex Foods',
+                address: 'New York',
+            };
+
+            await request(app).post('/api/v1/tenants').send(tenantData);
+
+            const tenantRepository = connection.getRepository(Tenant);
+            const tenants = await tenantRepository.find();
+            const tenant = tenants[0];
+
+            expect(tenants).toHaveLength(1);
+            expect(tenant).not.toBeNull();
+            expect(tenant.name).toBe(tenantData.name);
+            expect(tenant.address).toBe(tenantData.address);
         });
     });
 });

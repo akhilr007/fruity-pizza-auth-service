5869a3c69f909570aafcc13531ec6fbbef30bb9b
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-08 20:57:38 +0530

    add test for unauthorized tenant creation

--- src/routes/v1/tenant.ts
+++ src/routes/v1/tenant.ts
@@ -2,14 +2,19 @@ import { Router } from 'express';
 
 import logger from '../../configs/logger';
 import { TenantController } from '../../controllers/TenantController';
+import authenticate from '../../middlewares/authenticate';
 import { tenantRepository } from '../../repositories/tenant.repository';
+import { tenantSchema } from '../../schemas/tenant-schema';
 import { TenantService } from '../../services/TenantService';
+import { validateData } from '../../validators/validateData';
 
 const router = Router();
 
 const tenantService = new TenantService(tenantRepository, logger);
 const tenantController = new TenantController(tenantService, logger);
 
-router.post('/', (req, res, next) => tenantController.create(req, res, next));
+router.post('/', authenticate, validateData(tenantSchema), (req, res, next) =>
+    tenantController.create(req, res, next),
+);
 
 export default router;
--- tests/tenants/create.test.ts
+++ tests/tenants/create.test.ts
@@ -1,22 +1,31 @@
 import { StatusCodes } from 'http-status-codes';
+import createJWKSMock from 'mock-jwks';
 import request from 'supertest';
 import { DataSource } from 'typeorm';
 
 import app from '../../src/app';
 import { AppDataSource } from '../../src/configs/data-source';
+import { Roles } from '../../src/constants';
 import { Tenant } from '../../src/entity/Tenant';
 
 describe('POST /api/v1/tenants', () => {
     let connection: DataSource;
+    let jwksMock: ReturnType<typeof createJWKSMock>;
 
     beforeAll(async () => {
+        jwksMock = createJWKSMock('http://localhost:5555');
         connection = await AppDataSource.initialize();
     });
 
     beforeEach(async () => {
         // Database truncate
+        jwksMock.start();
         await connection.dropDatabase();
         await connection.synchronize();
+    });
+
+    afterEach(async () => {
+        jwksMock.stop();
     });
 
     afterAll(async () => {
@@ -30,8 +39,14 @@ describe('POST /api/v1/tenants', () => {
                 address: 'New York',
             };
 
+            const accessToken = jwksMock.token({
+                sub: '1',
+                role: Roles.ADMIN,
+            });
+
             const response = await request(app)
                 .post('/api/v1/tenants')
+                .set('Cookie', [`accessToken=${accessToken}`])
                 .send(tenantData);
 
             expect(response.statusCode).toBe(StatusCodes.CREATED);
@@ -43,7 +58,15 @@ describe('POST /api/v1/tenants', () => {
                 address: 'New York',
             };
 
-            await request(app).post('/api/v1/tenants').send(tenantData);
+            const accessToken = jwksMock.token({
+                sub: '1',
+                role: Roles.ADMIN,
+            });
+
+            await request(app)
+                .post('/api/v1/tenants')
+                .set('Cookie', [`accessToken=${accessToken}`])
+                .send(tenantData);
 
             const tenantRepository = connection.getRepository(Tenant);
             const tenants = await tenantRepository.find();
@@ -53,6 +76,23 @@ describe('POST /api/v1/tenants', () => {
             expect(tenant).not.toBeNull();
             expect(tenant.name).toBe(tenantData.name);
             expect(tenant.address).toBe(tenantData.address);
+        });
+
+        it('should return 401 if user is not authorized', async () => {
+            const tenantData = {
+                name: 'Alex Foods',
+                address: 'New York',
+            };
+
+            const response = await request(app)
+                .post('/api/v1/tenants')
+                .send(tenantData);
+
+            const tenantRepository = connection.getRepository(Tenant);
+            const tenants = await tenantRepository.find();
+
+            expect(tenants).toHaveLength(0);
+            expect(response.statusCode).toBe(StatusCodes.UNAUTHORIZED);
         });
     });
 });

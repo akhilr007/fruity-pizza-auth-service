5f09041706efb357bf30c0886110a66bc7e1c44c
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-08 14:29:46 +0530

    add routes for tenant

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
-import v1Router from './v1/auth';
+import v1AuthRouter from './v1/auth';
+import v1TenantRouter from './v1/tenant';
 
 const router = Router();
 
-router.use('/v1', v1Router);
+router.use('/v1/auth', v1AuthRouter);
+router.use('/v1/tenants', v1TenantRouter);
 
 export default router;

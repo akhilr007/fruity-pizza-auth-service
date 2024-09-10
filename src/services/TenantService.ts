7b72f5fdb33208c1ff63f4eb543de693e066360f
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-10 15:12:36 +0530

    update tenant service for find all

--- src/services/TenantService.ts
+++ src/services/TenantService.ts
@@ -58,32 +58,22 @@ export class TenantService {
     async findAll(
         validatedQuery: TenantQueryParams,
     ): Promise<[Tenant[], number]> {
-        try {
-            const queryBuilder =
-                this.tenantRepository.createQueryBuilder('tenant');
+        const queryBuilder = this.tenantRepository.createQueryBuilder('tenant');
 
-            if (validatedQuery.q) {
-                const searchTerm = `%${validatedQuery.q}%`;
-                queryBuilder.where(
-                    `CONCAT(tenant.name, ' ', tenant.address ILike :q)`,
-                    { q: searchTerm },
-                );
-            }
-
-            const result = await queryBuilder
-                .skip((validatedQuery.currentPage - 1) * validatedQuery.perPage)
-                .take(validatedQuery.perPage)
-                .orderBy('tenant.id', 'DESC')
-                .getManyAndCount();
-
-            return result;
-        } catch (error) {
-            this.logger.error(error);
-            throw createHttpError(
-                StatusCodes.INTERNAL_SERVER_ERROR,
-                'Failed to fetch all tenants',
+        if (validatedQuery.q) {
+            const searchTerm = `%${validatedQuery.q}%`;
+            queryBuilder.where(
+                "CONCAT(tenant.name, ' ', tenant.address) ILike :q",
+                { q: searchTerm },
             );
         }
+
+        const result = await queryBuilder
+            .skip((validatedQuery.currentPage - 1) * validatedQuery.perPage)
+            .take(validatedQuery.perPage)
+            .orderBy('tenant.id', 'DESC')
+            .getManyAndCount();
+        return result;
     }
 
     async deleteById(tenantId: number) {

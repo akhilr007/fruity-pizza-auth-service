c14257aa483e418b374cf23e729e87ad272bea2e
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-11 20:59:25 +0530

    fix ts build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     {
-        ignores: ['**/node_modules/**', 'dist/**'],
+        ignores: ['**/node_modules/**', 'dist/**', 'tests', '*.test.ts'],
     },
     {
         plugins: {
